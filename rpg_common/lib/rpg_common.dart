9e9ea3ed33db47991710676106312d67b94e4315
Dwayne Slater <ds84182@gmail.com>
2016-01-23 13:09:45 -0500

    Use Stagehand to generate the frontend, backend, and shared libraries.

---
+++ rpg_common/lib/rpg_common.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The rpg_common library.
+///
+/// This is an awesome library. More dartdocs go here.
+library rpg_common;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/rpg_common_base.dart';
