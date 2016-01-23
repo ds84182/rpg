90d8af689900a0e1a4685c8ad3c2550f0c07d802
Dwayne Slater <ds84182@gmail.com>
2016-01-23 14:09:16 -0500

    Start up pub serve from the server binary

--- rpg_server/bin/main.dart
+++ rpg_server/bin/main.dart
@@ -1,3 +1,13 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+
 main(List<String> args) {
-  print('Hello world!');
+  var executable = Platform.resolvedExecutable;
+
+  Process.start(executable,
+      [path.join(path.dirname(executable), "snapshots", "pub.dart.snapshot"), "serve"],
+      workingDirectory: Platform.script.resolve("../../rpg_client").path).then((process) {
+    stderr.addStream(process.stderr);
+    stdout.addStream(process.stdout);
+  });
 }
